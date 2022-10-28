46e8fd3b2c2f7f48cb07354b562c52e3369569c3
Damian Kijanczuk <kijanczukdamian@gmail.com>
2022-10-28 19:22:26 +0200

    Add 2 Antcedents

--- game3.py
+++ game3.py
@@ -1,7 +1,6 @@
 import pgzrun
 import random
 
-
 TITLE = "Arkanoid clone"
 WIDTH = 600
 HEIGHT = 500
@@ -40,7 +39,9 @@ def place_bars(x,y,image):
 def update():
     global ball_x_speed, ball_y_speed
 
-    val = fuzzyPaddleExpert.calculateMove(paddle.x - ball.x)
+    print(ball.x)
+
+    val = fuzzyPaddleExpert.calculateMove(paddle.x - ball.x, ball.x, ball.y)
 
     paddle.x = paddle.x + val
 
@@ -78,26 +79,42 @@ from skfuzzy import control as ctrl
 
 class FuzzyPaddleExpert():
     def __init__(self):
+        self.ball_X = ctrl.Antecedent(np.arange(-1, 601, 1), 'ball_X' )
+        self.ball_X[ 'leftSide'   ] = fuzz.trapmf(self.ball_X.universe, [-1, -1, 100, 100])
+        self.ball_X[ 'middle'   ] = fuzz.trimf(self.ball_X.universe, [-1, 300, 601])
+        self.ball_X[ 'rightSide'   ] = fuzz.trapmf(self.ball_X.universe, [550, 550, 601, 601])
+        self.ball_X.view()
+
+        self.ball_Y = ctrl.Antecedent(np.arange(-1, 501, 1), 'ball_Y' )
+        self.ball_Y[ 'high'   ] = fuzz.trimf(self.ball_Y.universe, [-1, -1, 401])
+        self.ball_Y[ 'low'   ] =  fuzz.trimf(self.ball_Y.universe, [-1, 501, 501])
+        self.ball_Y.view()
+
+
         self.paddle_Xdiff = ctrl.Antecedent(np.arange(-600, 610, 10), 'paddle_Xdiff' )
         self.paddle_Xdiff[ 'leftSide'   ] = fuzz.trapmf(self.paddle_Xdiff.universe, [-600,-600, 0,0])
         self.paddle_Xdiff[ 'rightSide'  ] = fuzz.trapmf(self.paddle_Xdiff.universe, [0,0,600, 600])
         self.paddle_Xdiff.view()
 
 
-        self.paddleMove = ctrl.Consequent(np.arange(-10, 11, 1), 'paddleMove' )
-        self.paddleMove[ 'left'  ] = fuzz.trapmf(self.paddleMove.universe, [-10,-10, 0, 0]  )
-        self.paddleMove[ 'right' ] = fuzz.trapmf(self.paddleMove.universe, [0,0, 10, 10]  )
+        self.paddleMove = ctrl.Consequent(np.arange(-8, 9, 1), 'paddleMove' )
+        self.paddleMove[ 'left'  ] = fuzz.trapmf(self.paddleMove.universe, [-8,-8, 0, 0]  )
+        self.paddleMove[ 'right' ] = fuzz.trapmf(self.paddleMove.universe, [0,0, 8, 8]  )
 
         rules = [
-            ctrl.Rule(self.paddle_Xdiff['leftSide'],     self.paddleMove['right']),
-            ctrl.Rule(self.paddle_Xdiff['rightSide'],     self.paddleMove['left']),
+            ctrl.Rule(self.paddle_Xdiff['leftSide'] & self.ball_X["middle"],     self.paddleMove['right']),
+            ctrl.Rule(self.paddle_Xdiff['rightSide']& self.ball_X["middle"],     self.paddleMove['left']),
+            ctrl.Rule(self.ball_X["leftSide"] &   self.ball_Y["high"], self.paddleMove['right']),
+            ctrl.Rule(self.ball_X["rightSide"]&   self.ball_Y["high"], self.paddleMove['left']),
         ]
 
         self.paddleControl = ctrl.ControlSystemSimulation(ctrl.ControlSystem(rules))
 
 
-    def calculateMove(self, paddle_Xdiff):
+    def calculateMove(self, paddle_Xdiff, ball_X, ball_Y):
         self.paddleControl.input['paddle_Xdiff'] = paddle_Xdiff
+        self.paddleControl.input['ball_X'] = ball_X
+        self.paddleControl.input['ball_Y'] = ball_Y
 
         self.paddleControl.compute()
         return self.paddleControl.output['paddleMove']
